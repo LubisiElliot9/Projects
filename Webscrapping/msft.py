00f1ba3859a127f15587f8230892a0b160bc8291
Elliot.lubisi9 <LubisiElliot9>
2022-03-22 20:20:57 +0200

    Edited the way we plot only ploting data for the previous year

--- Webscrapping/msft.py
+++ Webscrapping/msft.py
@@ -13,19 +13,27 @@ cf.go_offline()
 init_notebook_mode()
 
 msft = yf.Ticker("MSFT")
-history = msft.history(period="2y")
+history = pd.read_csv("Microsoft_data/Microsoft_Stock_data.csv")
+today_1 = msft.history(period="1d")
+data = history.append(today_1)
+data.to_csv("Data/Microsoft_Stock_data.csv")
 print("Data is ready!!")
+
+msft2 = yf.Ticker("MSFT")
+history = msft2.history(period="1y")
 
 history['Close'].plot(figsize=(16,9))
 plt.xlabel("Date")
 plt.ylabel("StockPrice")
 plt.title("Line Chart for MSFT stock Price")
-plt.show()
+plt.savefig("Charts/Linechart.png")
 
 
 qf = cf.QuantFig(history, title="Microsoft Stock Prices From 2020 March", name='MSFT')
 qf.add_sma(periods=10, column='Close', color='blue')
+qf.add_sma(periods=20, column='Close', color='red')
 qf.iplot()
+plt.savefig("Charts/CandleChart.png")
 
 
 
